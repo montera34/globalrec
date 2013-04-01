b58e8f2faa4098ecf4233367970848468ee55e01
numeroteca <pablo@basurama.org>
2013-04-01 10:54:43 -0400

    layout sidebar in blog + fix submenu items in main menu

--- page-blog.php
+++ page-blog.php
@@ -2,37 +2,41 @@
 get_header(); ?>
 
 <div class="container">
-<?php get_sidebar(); ?>
-	<div id="blog">	
-	<ul class="thumbnails">	
-		<?php
-		global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-			//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
-			$args = array(
-			 'caller_get_posts' => 1
-				);
-			if ( $paged > 1 ) {
-			 $args['paged'] = $paged;
-				}
+	<div class="row-fluid">
+		<div class="span3">
+			<?php get_sidebar(); ?>
+		</div>
+		<div id="blog" class="span9">	
+			<ul class="thumbnails">	
+			<?php
+			global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
+				//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
+				$args = array(
+				 'caller_get_posts' => 1
+					);
+				if ( $paged > 1 ) {
+				 $args['paged'] = $paged;
+					}
 
-			$my_query = new WP_Query($args);
+				$my_query = new WP_Query($args);
+				?>
+
+			<?php if ($my_query->have_posts() ) : 
+			$count = 0;
+			while ( $my_query->have_posts()) : $my_query->the_post(); 
+			$count++;
+			if ( $count == 1 ) { echo "<div class='row'>"; }
 			?>
+				<?php include("loop.boxes.php")?>
 
-		<?php if ($my_query->have_posts() ) : 
-		$count = 0;
-		while ( $my_query->have_posts()) : $my_query->the_post(); 
-		$count++;
-		if ( $count == 1 ) { echo "<div class='row'>"; }
-		?>
-		<?php include("loop.boxes.php")?>
+			<?php if ( $count == 2 ) { echo "</div><!-- .row -->"; $count = 0; }?>
 
-		<?php if ( $count == 2 ) { echo "</div><!-- .row -->"; $count = 0; }?>
-
-		<?php endwhile; else: ?>
-		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-		<?php endif; ?>
-	</ul>
-	<div class="navigation">
+			<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+			</ul>
+		</div>
+		<div class="navigation">
   			<div class="alignleft"><?php 
 				if ( !$max_page ) {
  					 $max_page = $my_query->max_num_pages;
@@ -54,8 +58,8 @@ get_header(); ?>
   				echo '<a href="' . previous_posts( false ) . "\" $attr>". preg_replace( '/&([^#])(?![a-z]{1,8};)/', '&$1', 'Newer posts &raquo;' ) .'</a>';
 					}
 				?></div>
-				</div>
-	</div><!-- #blog -->
-</div>	<!-- #main -->
-</div><!-- #content -->
-<?php get_footer(); ?>
+			</div>
+		</div>
+	<?php get_footer(); ?>
+</div>
+

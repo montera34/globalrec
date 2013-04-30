9b7a3e83f5d90c49b7d69d0d69d8df549fd72bc4
numeroteca <pablo@basurama.org>
2013-04-30 12:12:52 -0400

    adds carousel to home page, removes sticly posts, comments widget banner in front page

--- home.php
+++ home.php
@@ -6,12 +6,68 @@ $output = '';
 <div class="container">
 	<div class="row-fluid">
 		<div class="span9">
-			<div class="row-fluid"> <!-- space for banners -->
-				<div class="span12">
+			<!-- Carousel starts -->
+			<div class="row-fluid">
+				<div id="myCarousel" class="carousel slide">
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+						<li data-target="#myCarousel" data-slide-to="3"></li>
+						<li data-target="#myCarousel" data-slide-to="4"></li>
+					</ol>
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+					  <?php
+					   $the_query = new WP_Query(array(
+					    'category_name' => 'incineration', //ToDo, change it to taxonomy activated
+					    'posts_per_page' => 1
+					    ));
+					   while ( $the_query->have_posts() ) :
+					   $the_query->the_post();
+					  ?>
+					   <div class="active item">
+					    <?php the_post_thumbnail('large');?>
+						<div class="carousel-caption">
+			              <h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h4>
+			            </div>
+					   </div><!-- item active -->
+					  <?php
+					   endwhile;
+					   wp_reset_postdata();
+					  ?>
+					  <?php
+					   $the_query = new WP_Query(array(
+					    'category_name' => 'incineration', //ToDo, change it to taxonomy activated
+					    'posts_per_page' => 4,
+					    'offset' => 1
+					    ));
+					   while ( $the_query->have_posts() ) :
+					   $the_query->the_post();
+					  ?>
+					   <div  class="item">
+					    <?php the_post_thumbnail('large');?>
+						<div class="carousel-caption">
+			              <h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h4>
+			            </div>
+					   </div><!-- item -->
+					  <?php
+					   endwhile;
+					   wp_reset_postdata();
+					  ?>
+					 </div><!-- carousel-inner -->
+					<!-- Carousel nav -->
+					  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+					  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+					</div><!-- #myCarousel -->
+			</div>
+
+			<!-- div class="row-fluid"> <!-- space for banners -->
+				<!-- <div class="span12">
 					<?php dynamic_sidebar( 'front-page-widget-area-main' ) ?>
 				</div>
-			</div>
-			<hr>
+			</div>-->
+
 			<div class="row-fluid" id="home-boxes"> 
 				<!-- box for Life and voices link. It loads a random photo of a waste picker everytime.--------------------- -->
 				<div class="span4">
@@ -102,80 +158,10 @@ $output = '';
 				</div>
 			</div> 
 			<hr>
-			<div id="front-1" class="row-fluid"> <!-- sticky posts -->
-				<?php global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-
-				/* Get all sticky posts */
-				$sticky = get_option( 'sticky_posts' );
-				/* Sort the stickies with the newest ones at the top */
-				rsort( $sticky );
-				/* Get the 5 newest stickies (change 5 for a different number) */
-				$sticky = array_slice( $sticky, 0, 1 );
-
-
-				//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
-				$args = array(
-				 'caller_get_posts' => 1,
-				 'post__in' => $sticky, /* Query sticky posts */
-				 'posts_per_page'=>5	
-					);
-				if ( $paged > 1 ) {
-				 $args['paged'] = $paged;
-					}
-
-				$my_query = new WP_Query($args);
-				?>
-	
-				<?php if ( $my_query->have_posts() ) : ?><?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
-				<?php 	 global $wp_query; //necessary to show the tags 
-					$wp_query->in_the_loop = true; ?>
-				<?php $more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class('well span12'); ?>	>
-					<div class="row-fluid">
-							<div class="span5">
-							<?php if ( has_post_thumbnail()) : ?>
-							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-							<?php the_post_thumbnail('medium'); ?>
-							</a>
-						</div>
-						<?php endif; ?> 
-						<div class="span7">
-							<h4 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
-							<div class="post-metadata">
-							<small>
-							by <?php the_author_posts_link(); ?> | 
-							<?php  
-								if (get_the_term_list( $post->ID, 'post-region', '', ', ', '' ) != '')  : 
-								echo "Region ";	
-								echo get_the_term_list( $post->ID, 'post-region', '', ', ', '' ); 
-								endif;
-						 	?>
-							| <?php the_time('F d, Y') ?>
-							</small>				
-							</div>
-							<div>
-							<?php the_excerpt(); ?>
-							</div>	
-							<?php if($categories){
-							foreach($categories as $category) {
-								$output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '" class="label  pull-right">'.$category->cat_name.'</a>'.$separator;
-								}
-								echo trim($output, $separator);
-								}	
-								?>
-						</div>
-					</div>
-					<?php endwhile; else: ?>
-					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-					<?php endif; ?>
-				</div>
-			</div>
 			<div class="row-fluid">
 				<div class="span4">
 					<h3><a href="blog" title="Go to blog" alt="Go to blog">Organizing</a></h3>
-					<?php // montamos otro loop para llamar a los no sticky
-					global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-					//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
+					<?php global $more; 
 					$args = array(
 					 'caller_get_posts' => 	1,
 					 'posts_per_page'=>	4,

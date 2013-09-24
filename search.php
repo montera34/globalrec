78b3c807ea41b87fb39d54c51a16cffe72606713
numeroteca <pablo@basurama.org>
2013-09-24 18:49:23 +0200

    Fix search results and change it to 4 columns.

--- search.php
+++ search.php
@@ -1,47 +1,27 @@
-<?php get_header();
-
-$tit = "Search results";
-?>
-
-
-<div id="gallery-tit" class="row">
-	<div class="container">
-		<div class="row">
-			<div class="span3"><h2><?php echo $tit ?></h2></div>
+<?php get_header(); ?>
+<div class="container">
+	<div class="row-fluid">
+		<div class="row-fluid">
+			<div class="span6"><h2>Search results: <?php the_search_query(); ?></h2></div>
+			<div class="span6">Keep searching: <?php get_search_form(); ?></div>
+			<br>
 		</div>
-	</div>
-</div><!-- #gallery-tit -->
-<div id="gallery-items" class="row">
-	<div class="container">
 		<ul class="thumbnails">	
-			<?php
-			global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-				//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
-				$args = array(
-				 'caller_get_posts' => 1,
-				'posts_per_page'=>	12
-					);
-				if ( $paged > 1 ) {
-				 $args['paged'] = $paged;
-					}
-				$my_query = new WP_Query($args);
-				?>
-
-			<?php if ($my_query->have_posts() ) : 
+			<?php if (have_posts() ) : 
 			$count = 0;
-			while ( $my_query->have_posts()) : $my_query->the_post(); 
+			while (have_posts()) : the_post(); 
 			$count++;
 			if ( $count == 1 ) { echo "<div class='row-fluid'>"; }
 			?>
-			<li id="post-<?php the_ID(); ?>" <?php post_class('span4'); ?>	>
+			<li id="post-<?php the_ID(); ?>" <?php post_class('span3'); ?>	>
 				<?php include("loop.boxes.php")?>
 			</li>
-			<?php if ( $count == 3 ) { echo "</div><!-- .row --><hr>"; $count = 0; }?>
+			<?php if ( $count == 4 ) { echo "</div><!-- .row --><hr>"; $count = 0; }?>
 
 			<?php endwhile; else: ?>
 			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 			<?php endif; ?>
 		</ul>
-	</div><!-- .container -->
-</div><!-- #gallery-items -->
+	</div>
+</div> 
 <?php get_footer(); ?>

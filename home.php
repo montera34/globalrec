40d04e0c251adbc5bc843e931a1bd4a18a411694
numeroteca <pablo@basurama.org>
2013-10-15 21:46:37 +0200

    Column titles and sections in home are translated when change language

--- home.php
+++ home.php
@@ -6,34 +6,26 @@ $output = '';
 <div class="container">
 	<div class="row-fluid">
 		<div class="span9">
-			<!-- Carousel starts -->
-<script type="text/javascript">
-$(document).ready(function () {
-    $('.carousel').carousel({
-        interval: 3000
-    });
-
-    $('.carousel').carousel('cycle');
-});
-</script>
 			<!-- div class="row-fluid"> <!-- space for banners -->
 				<!-- <div class="span12">
 					<?php dynamic_sidebar( 'front-page-widget-area-main' ) ?>
 				</div>
 			</div>-->
-
 			<div class="row-fluid" id="home-boxes"> 
+				<!-- box for Life and Voices ----------------------------------------------------------->
 				<div class="span4">
-					<a href="/life-and-voices/">
-						<img src="<?php bloginfo('template_url'); ?>/images/wastepicker-faces_p.png" alt=">Life and Voices of Waste pickers" />
-						<h3 style="clear: left;">Life and Voices of Waste pickers</h3>
-						<p>Get to know waste picker leaders who have actively participated in the Global Alliance of Waste Pickers process.
+					<a href="<?php echo get_permalink(icl_object_id(2856,'page')) ?>">
+						<img src="<?php bloginfo('template_url'); ?>/images/wastepicker-faces_p.png" alt="Life and Voices of Waste pickers" />
+						<h3><?php icl_link_to_element(2856,'page'); ?></h3>
 					</a>
+					<p>Get to know waste picker leaders who have actively participated in the Global Alliance of Waste Pickers process.
 				</div>
 				<!-- box for Where we are? ----------------------------------------------------------->
 				<div class="span4">
-						<?php icl_link_to_element(7618,'page',__('<img src="http://globalrec.org/wp-content/themes/globalrec/images/map-waste-pickers-groups_p.png" >')); ?>
-						<h3><?php icl_link_to_element(7618,'page'); ?></h3>
+						<a href="<?php echo get_permalink(icl_object_id(7618,'page')) ?>">
+							<img src="<?php bloginfo('template_url'); ?>/images/map-waste-pickers-groups_p.png"/>
+							<h3><?php icl_link_to_element(7618,'page'); ?></h3>
+						</a>
 						<p>Provisional list of hundreds of waste pickers’ groups around the globe.</p>
 				</div>
 				<!-- box for the Last newsletter ----------------------------------------------------------->
@@ -43,10 +35,7 @@ $(document).ready(function () {
 						'post_type' => 'newsletter', 
 						'posts_per_page' => 1, 
 						'post_parent' => 0
-						//'order' =>  'ASC',
-						
 						);
-
 
 						if ( $paged > 1 ) {
 						 $args['paged'] = $paged;
@@ -62,29 +51,25 @@ $(document).ready(function () {
 						$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
 						<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), medium, false, '' ); ?>
 						<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-							<!--div class="size-thumbnail wp-image-2864 alignleft" style="margin:0 15px 15px 0;position: 
-							relative;width:200px;height:130px;background-image:url('<?php echo $src[0]; ?>');"-->
 							<?php the_post_thumbnail( 'medium' ); ?> 
-							<!--/div-->
 								<h4><?php the_title();?></h4>
 						</a> 	
 						<p>Check out the latest GlobalRec newsletter. You can also <a href="/subscription/">subscribe to receive it by email</a>.</p>
 						<?php endwhile; else: ?>
 						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 						<?php endif; ?>
-					<h3></h3>
 				</div>
 			</div> 
 			<hr>
 			<div class="row-fluid">
 				<div class="span4">
-					<h3><a href="/category/organizing/" title="Go to Organizing Articles">Organizing</a></h3>
+					<!-- Organizing post column -->
+					<h3><?php icl_link_to_element(858, 'category'); ?></h3>
 					<?php global $more; 
 					$args = array(
 					 'caller_get_posts' => 	1,
 					 'posts_per_page'=>	4,
-					 'cat'=>		858, //change to category "organizing" and internationalize
-					 //'post__not_in' => 	get_option( 'sticky_posts' )					 
+					 'cat'=> 858
 						);
 					if ( $paged > 1 ) {
 					 $args['paged'] = $paged;
@@ -106,20 +91,20 @@ $(document).ready(function () {
 					<?php endif; ?>
 				</div>
 				<div class="span4">
-					<h3><a href="category/threats/" title="Go to Threats Articles">Threats</a></h3>
-					<?php // montamos otro loop para llamar a los no sticky
-					global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
+					<!-- Threats posts column -->
+					<h3><?php icl_link_to_element(964, 'category'); ?></h3>
+					<?php global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
 					//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
 					$args = array(
 					 'caller_get_posts' => 	1,
 					 'posts_per_page'=>	4,
-					 'cat'=>		964, //change to category "threats" and internationalize				 
+					 'cat'=>	icl_object_id(964, 'category')		 
 						);
 					if ( $paged > 1 ) {
 					 $args['paged'] = $paged;
 						}
 		 
-					$my_query = new WP_Query($args); ?>
+					$my_query = new 	WP_Query($args); ?>
 					<ul class="thumbnails">
 						<?php if ( $my_query->have_posts() ) : 	while ( $my_query->have_posts() ) : $my_query->the_post(); 
 							
@@ -135,14 +120,14 @@ $(document).ready(function () {
 					<?php endif; ?>
 				</div>	
 				<div class="span4">
-					<h3><a href="/category/publications/" title="Go to Publications Articles">Publications</a></h3>
-					<?php // montamos otro loop para llamar a los no sticky
-					global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
+					<!-- Publications posts column -->
+					<h3><?php icl_link_to_element(970, 'category'); ?></h3>
+					<?php global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
 					//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
 					$args = array(
 					 'caller_get_posts' => 	1,
 					 'posts_per_page'=>	4,
-					 'cat'=>		970, //change to category "publications" and internationalize					 
+					 'cat'=> 970
 						);
 					if ( $paged > 1 ) {
 					 $args['paged'] = $paged;

0660a675ad0b36ada38d316f21c3e3f3268573f0
numeroteca <pablo@basurama.org>
2013-10-15 22:25:10 +0200

    More compact search form.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get" class="form-search" style="margin: 0 0 8px;">
+<form action="/" method="get" class="form-search" style="margin: 0 0 0px;">
         <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" class="input-small search-query" />
 	<label for="search" class="btn btn-small"><?php _e("Search"); ?></label>
 </form>

eac05abe0c5e3e6e3a8907a1faf1249034621a9a
numeroteca <pablo@basurama.org>
2013-11-15 00:19:22 +0100

    Adds globalrec twitter feed to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,9 @@
 <!-- begin main sidebar -->
 <div id="main-sidebar" class="nav nav-tabs nav-stacked">
 		<?php dynamic_sidebar( 'main-sidebar-widget' ) ?>
+		<a class="twitter-timeline"  href="https://twitter.com/numeroteca/globalrec"  data-widget-id="401042762868326400">Tweets from @numeroteca/globalrec</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
 </div>
 <!-- end main sidebar -->

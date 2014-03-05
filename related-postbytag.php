1bea8e00b904e528943cf29de7e61103eaa4ecd0
numeroteca <pablo@basurama.org>
2014-03-06 00:46:33 +0100

    Remove "caller posts" by  "ignore sticky posts"

--- related-postbytag.php
+++ related-postbytag.php
@@ -1,20 +1,19 @@
-<?php //wp_reset_query(); ?>
-
 <?php $prueba=get_post_meta($post->ID, 'gm_tag', true); 
 if ( $prueba) { ?>
 
 <?php // show related post //
-	global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
+	global $more; // Declare global $more (before the loop). "para que seguir leyendo funcione"
 			
 	$tagnow = get_post_meta($post->ID, 'gm_tag', true);
  
 	//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
 	$args = array(
-	 'caller_get_posts' => 1,
+	 'ignore_sticky_posts' => 1,
 	 'tag' => $tagnow
 		);
 	if ( $paged > 1 ) {
-	 $args['paged'] = $paged;}
+	 $args['paged'] = $paged;
+	 }
 
 	$my_query = new WP_Query($args);
 				 
@@ -24,9 +23,7 @@ if ( $prueba) { ?>
 		$wp_query->in_the_loop = true;
 		$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
 
-
 		<div id="post-<?php the_ID(); ?>">
-		
 			<h5 class="">
 				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
 					<?php the_title(); ?>

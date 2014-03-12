9a7ea96a3ffa9d65239f407eb3046efbc8165033
numeroteca <pablo@basurama.org>
2014-03-13 00:06:04 +0100

    Cleans global meetings page from unnecessary junk

--- page-global-meeting.php
+++ page-global-meeting.php
@@ -32,22 +32,6 @@ get_header(); ?>
 		<?php 	 //necessary to show the tags 
 			global $wp_query;
 			$wp_query->in_the_loop = true;
-			$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. 
-
-			//intento de añadir la class 'label' a los bg-type
-			$id = $post->ID;
-			$tax = 'gb-type';
-			$op = '';
-			$list = get_the_term_list($id,$tax,'','|','');
-			if ($list) {
-			   $terms = explode('|',$list);
-			   $op = '';
-			   foreach ($terms as $term) {
-				  $class = 'item-' . ++$i;
-				  $item = "<span class=\"label\">$term</span>";
-				  $op .= $item;
-			   }
-			}
 			?>		
 				<div id="post-<?php the_ID(); ?>" <?php post_class('col-md-4'); ?>	>
 					<?php include("loop.boxes.php")?>
@@ -72,7 +56,6 @@ get_header(); ?>
 			$my_query = new WP_Query($args);
 			?>
 
-
   <table class="table table-hover table-condensed">
 	<thead>
 		<tr>
@@ -84,11 +67,8 @@ get_header(); ?>
 	</thead>
     <tbody>
 	<?php if ( $my_query->have_posts() ) : while ( $my_query->have_posts() ) :  $my_query->the_post(); ?>
-	<?php 	 //necessary to show the tags 
-		global $wp_query;
+	<?php global $wp_query;
 		$wp_query->in_the_loop = true;
-		$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. 
-		
 
 		//intento de añadir la class 'label' a los bg-type
 		$id = $post->ID;
@@ -99,23 +79,24 @@ get_header(); ?>
 		   $terms = explode('|',$list);
 		   $op = '';
 		   foreach ($terms as $term) {
-		      $class = 'item-' . ++$i;
 		      $item = "<span class=\"label\">$term</span>";
 		      $op .= $item;
 		   }
 		}
 		?>
-
 			<tr <?php post_class('global-meeting-list'); ?> id="post-<?php the_ID(); ?>">
 				<td> <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-					<?php the_title(); ?></a><div class="edit-button"><?php // edit_post_link(__('Edit This')); ?></div> 
+					<?php the_title(); ?></a>
+			 		<?php if ( is_user_logged_in() ) { echo '<div class="btn btn-sm btn-default pull-right">'; edit_post_link(__('Edit This')); echo "</div>";} ?>
 				</td>
-				<td><?php $text = get_post_meta( $post->ID, 'gm_location', true ); echo $text; ?> </td>
 				<td>
-					<?php // echo get_the_term_list( $post->ID, 'gb-type', ' ', ', ', '' ); ?> 
+					<?php $text = get_post_meta( $post->ID, 'gm_location', true ); echo $text; ?>
+				</td>
+				<td>
 					<span class="label"><?php echo get_the_term_list( $post->ID, 'gb-year', ' ', ', ', '' ); ?></span> 
 				</td>
-				<td>	<?php echo $op; ?>
+				<td>
+					<?php echo $op; ?>
 				</td>
 			</tr>
 
@@ -124,30 +105,5 @@ get_header(); ?>
 	<?php endif; ?>
     </tbody>
   </table>
-
-	<div class="navigation">
-		<div class="alignleft"><?php 
-			if ( !$max_page ) {
-				 $max_page = $my_query->max_num_pages;
-				}
-
-			if ( !$paged ) {
-				 $paged = 1;
-				}
-				$nextpage = intval($paged) + 1;
-
-			if ( !is_single() && ( empty($paged) || $nextpage <= $max_page) ) {
-					$attr = apply_filters( 'next_posts_link_attributes', '' );
-				 echo '<a href="' . next_posts( $max_page, false ) . "\" $attr>". preg_replace('/&([^#])(?![a-z]{1,8};)/', '&$1', ' &laquo; Previous posts') .'</a>';
-				}
-				?>
-		</div>
-		<div class="alignright"><?php 
-				if ( !is_single() && $paged > 1 ) {
-				$attr = apply_filters( 'previous_posts_link_attributes', '' );
-				echo '<a href="' . previous_posts( false ) . "\" $attr>". preg_replace( '/&([^#])(?![a-z]{1,8};)/', '&$1', 'Newer posts &raquo;' ) .'</a>';
-				}	?>
-		</div>
-	</div>
 </div>
 <?php get_footer(); ?>

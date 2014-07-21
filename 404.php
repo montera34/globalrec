8b0b1cf2e2d360340251661ae306e13b4192c51d
numeroteca <pablo@basurama.org>
2014-07-22 00:39:48 +0200

    Bootstrapizes 303 error page

--- 404.php
+++ 404.php
@@ -1,22 +1,16 @@
 <?php header("HTTP/1.1 404 Not Found"); ?>
 <?php get_header() ?>
-
-			<div class="hfeed">
-				<div id="post-0" class="post hentry p1">
-					<h3 class="entry-title"><?php _e('Nothing Found', 'blogtxt') ?></h3>
-					<div class="entry-content">
-						<p>error 404 <?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'blogtxt') ?></p>
-					</div>
-					<form id="searchform" method="get" action="<?php bloginfo('home') ?>">
-						<div>
-							<input id="s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true); ?>" tabindex="1" size="40" />
-							<input id="searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Search', 'blogtxt') ?>" tabindex="2" />
-						</div>
-					</form>
-				</div><!-- #post-0 .post -->
-			</div><!-- .hfeed -->
-		</div><!-- #content -->
-	</div><!-- #container -->
-
-<?php get_sidebar() ?>
+<article id="post-<?php the_ID(); ?>">
+	<div class="row">
+		<h2 id="post-<?php the_ID(); ?>" class="col-md-10">
+			error 404: <?php _e('Nothing Found', 'blogtxt') ?>
+		</h2>
+	</div>
+	<div class="row">
+		<div class="col-md-8 content">
+			<p><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'blogtxt') ?></p>
+			<?php get_search_form(); ?>
+		</div>
+	</div>
+</article>
 <?php get_footer() ?>

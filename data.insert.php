862957f13bcc1d6573f112a5d9f0faceac11f8dc
numeroteca <pablo@basurama.org>
2014-09-22 13:12:09 +0200

    Adds when inserting: Converts to lower case the multicheck options. Only inserts when value exists. Iterates through multiple value in multichecks inserts.

--- data.insert.php
+++ data.insert.php
@@ -40,7 +40,7 @@ get_header();
 					$facebook = $fp_csv[14]; // cf
 					$twitter = $fp_csv[15]; // cf
 					$other_social_networks = $fp_csv[16]; // cf
-					$language = explode(", ", $fp_csv[17]); // cf
+					$language = explode(", ", strtolower($fp_csv[17])); // cf
 					$primary_contact_name = $fp_csv[18]; // cf
 					$primary_contact_phone = $fp_csv[19]; // cf
 					$primary_contact_position = $fp_csv[20]; // cf
@@ -48,56 +48,45 @@ get_header();
 					$secondary_contact_name = $fp_csv[22]; // cf
 					$secondary_contact_phone = $fp_csv[23]; // cf
 					$secondary_contact_email = $fp_csv[24]; // cf
-					$members_type = explode(", ", $fp_csv[25]);
-					$members_occupation = explode(", ", $fp_csv[26]); // cf
-					$organization_type = explode(", ", $fp_csv[27]); // cf
-					$organization_scope = explode(", ", $fp_csv[28]); // cf
-					$workplace_members = explode(", ", $fp_csv[29]); // cf
+					$members_type = explode(", ", strtolower($fp_csv[25]));
+					$members_occupation = explode(", ", strtolower($fp_csv[26])); // cf
+					$organization_type = explode(", ", strtolower($fp_csv[27])); // cf
+					$organization_scope = explode(", ", strtolower($fp_csv[28])); // cf
+					$workplace_members = explode(", ", strtolower($fp_csv[29])); // cf
 					$membership = $fp_csv[30]; // cf
 					$number_groups = $fp_csv[31]; // cf
 					$number_individuals = $fp_csv[32]; // cf
 					$gender_women_composition = $fp_csv[33]; // cf
 					$gender_women_comment = $fp_csv[34]; // cf
 					$structure = $fp_csv[35]; // cf
-					$objectives = explode(", ", $fp_csv[36]); // cf
-					$education_training = explode(", ", $fp_csv[37]); // cf
+					$objectives = explode(", ", strtolower($fp_csv[36])); // cf
+					$education_training = explode(", ", strtolower($fp_csv[37])); // cf
 					$formally_registered = $fp_csv[38]; // cf
 					//$xxx = $fp_csv[39]; // cf
 					$year_formed = $fp_csv[40]; // cf
 					$registration_year = $fp_csv[41]; // cf
 					$partnering_organizations = $fp_csv[42]; // cf
-					$affiliations = explode(", ", $fp_csv[43]); // cf
-					$funding = explode(", ", $fp_csv[44]); // cf
+					$affiliations = explode(", ", strtolower($fp_csv[43])); // cf
+					$funding = explode(", ", strtolower($fp_csv[44])); // cf
 					$elections = $fp_csv[45]; // cf
-					$member_benefits = explode(", ", $fp_csv[46]); // cf
+					$member_benefits = explode(", ", strtolower($fp_csv[46])); // cf
 					$credit_members = $fp_csv[47]; // cf
 					$safety_technology = explode(", ", $fp_csv[48]); // cf
-					$relationship_municipality = explode(", ", $fp_csv[49]); // cf
+					$relationship_municipality = explode(", ", strtolower($fp_csv[49])); // cf
 					$types_of_materials = explode(", ", $fp_csv[50]); // cf
 					$middlemen = $fp_csv[51]; // cf
-					$activities = explode(", ", $fp_csv[52]); // cf
+					$activities = explode(", ", strtolower($fp_csv[52])); // cf
 					$sorting_spaces = explode(", ", $fp_csv[53]); // cf
-					$treatment_organic_materials = explode(", ", $fp_csv[54]); // cf
-					$challenges_access_waste = explode(", ", $fp_csv[55]); // cf
+					$treatment_organic_materials = explode(", ", strtolower($fp_csv[54])); // cf
+					$challenges_access_waste = explode(", ", strtolower($fp_csv[55])); // cf
 					$content = $fp_csv[56]; // content
 					$publications = $fp_csv[57]; // cf
 					$information_source = $fp_csv[58]; // cf
 					$date_data_entry = $fp_csv[59]; // cf
 					$date_data_updated = $fp_csv[60]; // cf
 					$status	 = $fp_csv[61]; // cf
-					/*if ( $excerpt == '' ) { 
-						$excerpt == $content; 
-						}*/
 						
 						
-				//$members_type = explode(", ", $fp_csv[25]);
-
-				$multicheck = array(
-					 '_wpg_language' => $language,
-					 '_wpg_members_type' => $members_type,
-					 '_wpg_members_occupation' => $members_occupation,
-				);
-
 					$fields = array(
 						'_wpg_email' => $org_email,
 						'_wpg_website' => $website,
@@ -114,7 +103,6 @@ get_header();
 						'_wpg_skype' => $skype,
 						'_wpg_twitter' => $twitter,
 						'_wpg_other_social_networks' => $other_social_networks,
-						//'_wpg_language' => $language,
 						'_wpg_primary_contact_name' => $primary_contact_name,
 						'_wpg_primary_contact_phone' => $primary_contact_phone,
 						'_wpg_primary_contact_position' => $primary_contact_position,
@@ -122,36 +110,19 @@ get_header();
 						'_wpg_secondary_contact_name' => $secondary_contact_name,
 						'_wpg_secondary_contact_phone' => $secondary_contact_phone,
 						'_wpg_secondary_contact_email' => $secondary_contact_email,
-						//'_wpg_members_type' => $members_type,
-						//'_wpg_members_occupation' => $members_occupation,
-						//'_wpg_organization_type' => $organization_type,
-						//'_wpg_organization_scope' => $organization_scope,
-						//'_wpg_workplace_members' => $workplace_members,
 						'_wpg_membership' => $membership,
 						'_wpg_number_groups' => $number_groups,
 						'_wpg_number_individuals' => $number_individuals,
 						'_wpg_gender_women_composition' => $gender_women_composition,
 						'_wpg_gender_women_comment' => $gender_women_comment,
 						'_wpg_structure' => $structure,
-						//'_wpg_objectives' => $objectives,
-						//'_wpg_education_training' => $education_training,
 						'_wpg_formally_registered' => $formally_registered,
 						'_wpg_year_formed' => $year_formed,
 						'_wpg_registration_year' => $registration_year,
 						'_wpg_partnering_organizations' => $partnering_organizations,
-						//'_wpg_affiliations' => $affiliations,
-						//'_wpg_funding' => $funding,
 						'_wpg_elections' => $elections,
-						//'_wpg_member_benefits' => $member_benefits,
 						'_wpg_credit_members' => $credit_members,
-						//'_wpg_safety_technology' => $safety_technology,
-						//'_wpg_relationship_municipality' => $relationship_municipality,
-						//'_wpg_types_of_materials' => $types_of_materials,
 						'_wpg_middlemen' => $middlemen,
-						//'_wpg_activities' => $activities,
-						//'_wpg_sorting_spaces' => $sorting_spaces,
-						//'_wpg_treatment_organic_materials' => $treatment_organic_materials,
-						//'_wpg_challenges_access_waste' => $challenges_access_waste,
 						'_wpg_publications' => $publications,
 						'_wpg_information_source' => $information_source,
 						'_wpg_date_data_entry' => $date_data_entry,
@@ -196,12 +167,22 @@ get_header();
 						reset($fields);
 						// while ( $field = current($fields) ) {
 						foreach ( $fields as $key => $value ) {
-							add_post_meta($wpg, $key, $value, TRUE);
+							if ($value != '') {
+								add_post_meta($wpg, $key, $value, TRUE);
+							}
 						//	next($fields);
 						}
 						
 						foreach ( $multicheck as $key => $value ) {
-							update_post_meta($wpg, $key, $value);
+							if ($value != '') {
+								if (gettype($value) == "array") {
+										foreach ( $value as $val ) {
+											add_post_meta($wpg, $key, $val, FALSE);
+										}
+								} else {
+									add_post_meta($wpg, $key, $value, FALSE);
+								}
+							}
 						}
 					} // if project has been inserted
 					echo "

6bb1b7724b399922bd62022d6d68a992713c36cc
numeroteca <pablo@basurama.org>
2015-02-03 22:32:43 +0100

    Changes url of WAW database to /waw in nav menu

--- nav-waw.php
+++ nav-waw.php
@@ -1,21 +1,21 @@
 <ul class="nav nav-tabs" role="tablist">
-	<li role="presentation"><a href="/waste-picker-organizations/" title="Home WAW"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
-	<li role="presentation"><a href="/waste-picker-organizations/about">About WAW</a></li>
-	<li role="presentation"><a href="/waste-picker-organizations/stats"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Statistics</a></li>
-	<li role="presentation"><a href="/waste-picker-organizations/list"> 
+	<li role="presentation"><a href="/waw/" title="Home WAW"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+	<li role="presentation"><a href="/waw/about">About WAW</a></li>
+	<li role="presentation"><a href="/waw/stats"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Statistics</a></li>
+	<li role="presentation"><a href="/waw/list">
 		<span class="glyphicon glyphicon-list" aria-hidden="true"></span> List of Waste Picker Organizations</a>
 	</li>
-	<li><a href="/waste-picker-organizations/waste-picker-organizations-map">Waste Pickers' Organizations Map</a></li>
+	<li><a href="/waw/waste-picker-organizations-map">Waste Pickers' Organizations Map</a></li>
 	<li role="presentation" class="dropdown">
-		<a class="dropdown-toggle" data-toggle="dropdown" href="/waste-picker-organizations/" role="button" aria-expanded="false">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="/waw/" role="button" aria-expanded="false">
 		  <span class="glyphicon glyphicon-globe"></span> Maps <span class="caret"></span>
 		</a>
 		<ul class="dropdown-menu" role="menu">
-			<li role="presentation"><a href="/waste-picker-organizations/waste-picker-organizations-map">Waste Pickers' Organizations</a></li>
-			<li role="presentation"><a href="/waste-picker-organizations/oganizations-type">Type of Oganizations</a></li>
-			<li role="presentation"><a href="/waste-picker-organizations/scope">Scope</a></li>
-			<li role="presentation"><a href="/waste-picker-organizations/supporters">Supporters</a></li>
+			<li role="presentation"><a href="/waw/waste-picker-organizations-map">Waste Pickers' Organizations</a></li>
+			<li role="presentation"><a href="/waw/oganizations-type">Type of Oganizations</a></li>
+			<li role="presentation"><a href="/waw/scope">Scope</a></li>
+			<li role="presentation"><a href="/waw/supporters">Supporters</a></li>
 		</ul>
 	</li>
-	<li role="presentation"><a href="/waste-picker-organizations/add-organization">Add your organization</a></li>
+	<li role="presentation"><a href="/waw/add-organization">Add your organization</a></li>
 </ul>

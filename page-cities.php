98a9acb851da690990bb960dc4250ed0c81eaab9
numeroteca <pablo@basurama.org>
2016-03-15 22:53:51 +0100

    Makes list of countries with width 8. removes form list "-" and "not specified" from country list.

--- page-cities.php
+++ page-cities.php
@@ -4,7 +4,7 @@ get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post();?>
 		<div class="row">
 			<h2 id="post-<?php the_ID(); ?>" class="col-md-10">
-				<?php the_title();?>	
+				<?php the_title();?>
 			</h2>		
 			<div class="pull-right"><?php do_action('icl_language_selector'); ?></div>
 		</div>
@@ -24,51 +24,56 @@ get_header(); ?>
 				);
 			$my_query = new WP_Query($args);
 			?>
-	
-	<?php if ( $my_query->have_posts() ) : ?>
-  <table class="table table-hover table-condensed">
-	<thead>
-		<tr>
-			<th><?php _e('Cities','globalrec'); ?></th>
-			<th><?php _e('City Report: Interview with a local Waste Picker','globalrec'); ?></th>
-			<th><?php _e('Countries','globalrec'); ?></th>
-		</tr>
-	</thead>
-    <tbody>
-	<?php while ( $my_query->have_posts() ) :  $my_query->the_post(); ?>
-	<?php
-		global $wp_query;
-		$wp_query->in_the_loop = true;
-		?>
-
-			<tr>
-				<td> <a href="<?php the_permalink() ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>">
-					<?php the_title(); ?></a> 
-					<?php if ( is_user_logged_in() ) { ?><div class="btn btn-xs btn-default"> <?php edit_post_link(__('Edit This')); ?></div> <?php } ?>
-				</td>
-				<td>
-				<?php $content = get_the_content();
-					//only displays "check" if there is content, that means> there is a Interview city report
-					if ($content !='') {?>
-					 <a href="<?php the_permalink() ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>">	<span class="glyphicon glyphicon-ok"></span></a>
-					<?php } ?>
-				</td>
-				<td>
-				<?php //Country
-				$post_id = $post->ID;
-				$country_id = get_post_meta( $post_id, '_city_countryselect', true );
-				$country = get_post($country_id);
-				$country_link = get_permalink($country->ID);
-				$country_name = $country->post_title;
-				echo '<a href="'.$country_link.'">'.$country_name.'</a>';
-				?>
-				</td>
-			</tr>
-
-	<?php endwhile; else: ?>
-	<p><?php _e('Sorry, this part of the content is not available in this language'); ?></p>
-	<?php endif; ?>
-    </tbody>
-  </table>
+		<div class="row">
+			<div class="col-md-8">
+				<?php if ( $my_query->have_posts() ) : ?>
+				<table class="table table-hover table-condensed">
+					<thead>
+						<tr>
+							<th><?php _e('Cities','globalrec'); ?></th>
+							<th><?php _e('City Report: Interview with a local Waste Picker','globalrec'); ?></th>
+							<th><?php _e('Countries','globalrec'); ?></th>
+						</tr>
+					</thead>
+		 			<tbody>
+						<?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+						<?php
+						global $wp_query;
+						$wp_query->in_the_loop = true; ?>
+						<?php
+						  $posts_to_exclude = array(28963, 10943); //excludes "-" and "not specified" from list by they ID.
+  						if(!in_array($post->ID, $posts_to_exclude)):
+							?>
+							<tr>
+								<td> <a href="<?php the_permalink() ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>">
+									<?php the_title(); ?></a>
+									<?php if ( is_user_logged_in() ) { ?><div class="btn btn-xs btn-default"> <?php edit_post_link(__('Edit This')); ?></div> <?php } ?>
+								</td>
+								<td>
+								<?php $content = get_the_content();
+									//only displays "check" if there is content, that means> there is a Interview city report
+									if ($content !='') {?>
+									 <a href="<?php the_permalink() ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>">	<span class="glyphicon glyphicon-ok"></span></a>
+									<?php } ?>
+								</td>
+								<td>
+								<?php //Country
+								$post_id = $post->ID;
+								$country_id = get_post_meta( $post_id, '_city_countryselect', true );
+								$country = get_post($country_id);
+								$country_link = get_permalink($country->ID);
+								$country_name = $country->post_title;
+								echo '<a href="'.$country_link.'">'.$country_name.'</a>';
+								?>
+								</td>
+							</tr>
+						<?php endif; ?>
+						<?php endwhile; else: ?>
+						<p><?php _e('Sorry, this part of the content is not available in this language'); ?></p>
+						<?php endif; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
 </div>
 <?php get_footer(); ?>

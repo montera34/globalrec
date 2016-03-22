634bbdeb7e4dffde869a52b1c4a1c0eba9f85a73
numeroteca <pablo@basurama.org>
2016-03-22 15:27:41 +0100

    Coverts country list post page to panels and colours posts according to year of publication

--- page-country-posts.php
+++ page-country-posts.php
@@ -9,7 +9,9 @@ get_header(); ?>
 		$countries = get_posts( array(
 				'post_type' => 'country',
 				'posts_per_page'   => -1,
-				'suppress_filters'   => 0 //to force the use of current language
+				'suppress_filters'   => 0, //to force the use of current language
+				'orderby' => 'title',
+				'order' => 'ASC',
 			));
 
 		foreach($countries as $country) {
@@ -20,27 +22,40 @@ get_header(); ?>
 				'posts_per_page'   => -1
 			));
 			$count = count($posts);
+
 			if ($posts) {?>
-				<h6 class="document-dashicon">
-					<?php _e('Last updates from','globalrec'); ?>
-					<?php echo $country->post_title. " (" .$count. ")"; ?>
-				</h6>
-				<div class="list-group">
-					<?php
-					for ($i = 0; $i <= 2; $i++) {
-						if ( $i >= $count ) { } //to prevent crashing for countries with lesss than 2 posts
-						else {
-							$post = $posts[$i];
-							$published_date = get_post_meta( $post->ID, '_gr_article-date', true );
-							echo '<a href="'.get_permalink($post->ID).'" class="list-group-item">'.$post->post_title;
-							echo $published_date != ''? ' ('.$published_date.')</a>' : '</a>';
-						}
-					} ?>
-				</div><?php
-			}
+				<div class="panel panel-default">
+					<!-- Default panel contents -->
+					<div class="panel-heading">
+						<?php _e('Updates from','globalrec'); ?>
+						<strong><?php echo $country->post_title; ?></strong>
+						<span class="badge">
+							<?php echo  $count; ?>
+						</span>
+					</div>
+					<ul class="list-group">
+						<?php
+						for ($i = 0; $i <= 2; $i++) {
+							if ( $i >= $count ) { } //to prevent crashing for countries with lesss than 2 posts
+							else {
+								$post = $posts[$i];
+								$published_year = get_the_date('Y');
+								$published_date = get_post_meta( $post->ID, '_gr_article-date', true );
+								echo '<li class="list-group-item year-' .$published_year . '"><a href="'.get_permalink($post->ID).'">'. $post->post_title . get_the_date(' (m/Y)') .'</a></li>';
+								//echo $published_date != ''? ' ('.$published_date.')</a>' : '</a>';
+							}
+						} ?>
+					</ul>
+				</div>
+				<?php
+				} else { ?>
+				<h4 class="document-dashicon">
+					<?php _e('No updates from','globalrec'); ?>
+					<?php echo $country->post_title. " (0)"; ?>
+				</h4>
+		<?php }
 			wp_reset_query();
-		}
-		?>
+		} ?>
 		</div>
 	</div>
 </div>

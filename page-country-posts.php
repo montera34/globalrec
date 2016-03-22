1fae6c33617fb9f38be002141bf456327ee9eed4
numeroteca <pablo@basurama.org>
2016-03-22 11:49:03 +0100

    Limits number of posts per country to 3. Displays the number of posts per country (for country page list).

--- page-country-posts.php
+++ page-country-posts.php
@@ -2,35 +2,46 @@
 get_header(); ?>
 
 <div id="page-wpg">
-<?php //Posts about this Country
+	<div class="row">
+		<div class="col-md-8">
+		<?php //Posts about this Country
 
-$countries = get_posts( array(
-		'post_type' => 'country',
-		'posts_per_page'   => -1,
-		'suppress_filters'   => 0
-	));
+		$countries = get_posts( array(
+				'post_type' => 'country',
+				'posts_per_page'   => -1,
+				'suppress_filters'   => 0 //to force the use of current language
+			));
 
-foreach($countries as $country) {
-	$posts = get_posts( array(
-		'post_type' => 'post',
-		'meta_key' => '_post_country',
-		'meta_value' => $country->ID,
-		'posts_per_page'   => -1
-	));
-	if ($posts) {?>
-		<h3 class="document-dashicon">
-			<?php _e('Last updates from','globalrec'); ?>
-			<?php echo $country->post_title; ?></h3>
-		<div class="list-group">
-			<?php foreach($posts as $post) {
-				$published_date = get_post_meta( $post->ID, '_gr_article-date', true );
-				echo '<a href="'.get_permalink($post->ID).'" class="list-group-item">'.$post->post_title;
-				echo $published_date != ''? ' ('.$published_date.')</a>' : '</a>';
-			} ?>
-		</div><?php
-	}
-	wp_reset_query();
-}
-?>
+		foreach($countries as $country) {
+			$posts = get_posts( array(
+				'post_type' => 'post',
+				'meta_key' => '_post_country',
+				'meta_value' => $country->ID,
+				'posts_per_page'   => -1
+			));
+			$count = count($posts);
+			if ($posts) {?>
+				<h6 class="document-dashicon">
+					<?php _e('Last updates from','globalrec'); ?>
+					<?php echo $country->post_title. " (" .$count. ")"; ?>
+				</h6>
+				<div class="list-group">
+					<?php
+					for ($i = 0; $i <= 2; $i++) {
+						if ( $i >= $count ) { } //to prevent crashing for countries with lesss than 2 posts
+						else {
+							$post = $posts[$i];
+							$published_date = get_post_meta( $post->ID, '_gr_article-date', true );
+							echo '<a href="'.get_permalink($post->ID).'" class="list-group-item">'.$post->post_title;
+							echo $published_date != ''? ' ('.$published_date.')</a>' : '</a>';
+						}
+					} ?>
+				</div><?php
+			}
+			wp_reset_query();
+		}
+		?>
+		</div>
+	</div>
 </div>
 <?php get_footer(); ?>

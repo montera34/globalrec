f3e8c24c7d51257582891b10edac8d19065cffff
numeroteca <pablo@basurama.org>
2016-03-23 00:21:30 +0100

    Internationalizas search results page and adds pagination to access previous results

--- search.php
+++ search.php
@@ -1,26 +1,36 @@
 <?php get_header(); ?>
 <div id="search">
 	<div class="row">
-		<div class="row">
-			<div class="col-md-6"><h2>Search results: <?php the_search_query(); ?></h2></div>
-			<div class="col-md-2">Keep searching: </div>
-			<div class="col-md-4 pull-right"><?php get_search_form(); ?></div>				
-			<br>
+		<div class="col-md-8">
+			<h2>
+				<?php _e("Search results","globalrec") ?>: "<?php the_search_query(); ?>"
+				</h2>
 		</div>
-			<?php if (have_posts() ) : 
-			$count = 0;
-			while (have_posts()) : the_post(); 
-			$count++;
-			if ( $count == 1 ) { echo "<div class='row'>"; }
-			?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class('col-md-3'); ?>	>
-				<?php include("loop.boxes.php")?>
-			</div>
-			<?php if ( $count == 4 ) { echo "</div><!-- .row --><hr>"; $count = 0; }?>
+		<div class="col-md-4">
+			<?php // _e("Keep searching","globalrec") ?>
+			 <?php get_search_form(); ?>
+		</div>
+	</div>
+		<?php if (have_posts() ) :
+		$count = 0;
+		while (have_posts()) : the_post();
+		$count++;
+	if ( $count == 1 ) { echo "<div class='row'>"; }
+		?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class('col-md-3'); ?>	>
+			<?php include("loop.boxes.php")?>
+		</div>
+	<?php if ( $count == 4 ) { echo "</div><!-- .row --><hr>"; $count = 0; }?>
 
-			<?php endwhile; else: ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			<?php endif; ?>
+		<?php endwhile; else: ?>
+		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+		<?php endif; ?>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="pull-right">
+				<?php echo custom_pagination(); ?>
+			</div>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>

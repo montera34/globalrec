8f639a7a0cfaee59b07bc1500940cb5c3a4261e8
skotperez <voraginebramante@gmail.com>
2017-02-03 19:40:33 +0100

    Simplify and optimize loops in home

--- home.php
+++ home.php
@@ -30,119 +30,66 @@ $output = '';
 				</div>
 				<!-- box for the Last newsletter -->
 				<div class="col-xs-12 col-sm-6 col-md-4">
-					<?php global $more;   
+					<?php //global $more;   
 					$args = array(		//arguments for showing newsletters custom post type
 						'post_type' => 'newsletter', 
 						'posts_per_page' => 1, 
-						'post_parent' => 0
+						//'post_parent' => 0
 						);
-
-						if ( $paged > 1 ) {
-						 $args['paged'] = $paged;
-							}
-
-						$my_query = new WP_Query($args);
-	
-						if ( $my_query->have_posts() ) :  while ( $my_query->have_posts() ) :  $my_query->the_post();  
-							//necessary to show the tags 
-							global $wp_query;
-							$wp_query->in_the_loop = true;
-	
-						$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
-						<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-							<?php // the_post_thumbnail( 'medium', array('class' => 'img-responsive') ); //inserts the medium size image ?> 
-							<?php $image_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'medium', false, '' ); ?>
-							<div style="background-size:100% auto;background-image:url('<?php echo $image_attributes[0] ;?>');height:130px;margin-bottom: 4px;max-width:300px;background-repeat:no-repeat;background-position: center;"></div>
+						$newsletters = get_posts($args);
+						foreach ( $newsletters as $n ) {
+							setup_postdata( $n ); ?>
+							<a href="<?php the_permalink($n) ?>" rel="bookmark" title="<?php the_title_attribute(array( 'before' => '', 'after' => '', 'echo' => TRUE, 'post' => $n )); ?>">
+								<?php $image_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $n->ID), 'medium', false, '' ); ?>
+								<div style="background-size:100% auto;background-image:url('<?php echo $image_attributes[0] ;?>');height:130px;margin-bottom: 4px;max-width:300px;background-repeat:no-repeat;background-position: center;"></div>
 								<h3><?php _e('Recent newsletter','globalrec'); ?></h3>
-						</a> 	
-						<p><?php _e('Check out the latest GlobalRec newsletter.','globalrec'); ?> <a href="<?php echo get_permalink(icl_object_id_check(4491,'page')) ?>"><?php _e('You can also subscribe to receive it by email','globalrec'); ?></a>. <a href="<?php echo get_permalink(icl_object_id_check(4491,'page')) ?>"><button class="btn btn-xs btn-default"> <?php _e('Subscribe to newsletter','globalrec'); ?></button></a></p>
-						<?php endwhile; else: ?>
-						<p><?php _e('Sorry, no posts matched your criteria.','globalrec'); ?></p>
-						<?php endif; ?>
-						
+							</a> 
+							<p><?php _e('Check out the latest GlobalRec newsletter.','globalrec'); ?> <a href="<?php echo get_permalink(icl_object_id_check(4491,'page')) ?>"><?php _e('You can also subscribe to receive it by email','globalrec'); ?></a>. <a href="<?php echo get_permalink(icl_object_id_check(4491,'page')) ?>"><button class="btn btn-xs btn-default"> <?php _e('Subscribe to newsletter','globalrec'); ?></button></a></p>
+						<?php } wp_reset_postdata(); ?>
 				</div>
 			</div> 
 			<hr style="margin-top:5px;margin-bottom:0px;">
 			<div class="row" >
 				<div class="col-md-12">
 				<h2><?php _e('News','globalrec'); ?><small> <?php _e('from Waste Pickers around the world','globalrec'); ?></small></h2>
-				</div>			
+				</div>
 			</div>
 			<div class="row" id="home-boxes-2">
-				<div class="col-md-4">
-					<!-- Organizing post column -->
-					<h3><img src="<?php bloginfo('template_url'); ?>/images/icon-hand-in-hand.png"/><br><?php icl_link_to_element_check(858, 'category'); ?></h3>
-					<?php global $more;
+				<?php $categs = array(
+					'858' => array( // Organizing
+						'img' => 'icon-hand-in-hand.png',
+						'number_of_posts' => '5'
+					),
+					'964' => array( // Threats
+						'img' => 'icon-march.png',
+						'number_of_posts' => '5'
+					),
+					'970' => array( // Publications
+						'img' => 'icon-publication.png',
+						'number_of_posts' => '5'
+					)
+				);
+				foreach ( $categs as $id => $c ) {
 					$args = array(
 					 'ignore_sticky_posts' => 	0,
-					 'posts_per_page'=>	5,
-					 'cat'=> icl_object_id_check(858, 'category')
-						);
-					$my_query = new WP_Query($args); ?>
-						<?php if ( $my_query->have_posts() ) : 	while ( $my_query->have_posts() ) : $my_query->the_post();
-						$do_not_duplicate[] = $post->ID; //stores posts of this query in array
+					 'posts_per_page'=> $c['number_of_posts'],
+					 'cat'=> icl_object_id_check($id, 'category')
+					);
+					$my_query = new WP_Query($args);
+					if ( $my_query->have_posts() ) : ?>
+					<div class="col-md-4">
+						<h3><img src="<?php bloginfo('template_url'); echo "/images/".$c['img']; ?>" /><br><?php icl_link_to_element_check($id, 'category'); ?></h3>
 						
-						global $wp_query;	 //necessary to show the tags 
-							$wp_query->in_the_loop = true; 
-							$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
-							<div id="post-<?php the_ID(); ?>" <?php post_class(''); ?>	>
-								<?php include("loop.boxes.php")?>
-							</div>
-						<?php endwhile; else: ?>
-					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-					<?php endif; ?>
-				</div>
-				<div class="col-md-4">
-					<!-- Threats posts column-->
-					<h3><img src="<?php bloginfo('template_url'); ?>/images/icon-march.png"/><br><?php icl_link_to_element_check(964, 'category'); ?></h3>
-					<?php global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-					//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
-					$args = array(
-					 'ignore_sticky_posts' => 	0,
-					 'posts_per_page' =>	5,
-					 'cat' =>	icl_object_id_check(964, 'category'),
-					 'post__not_in' => $do_not_duplicate,
-						);
-		 
-					$my_query = new WP_Query($args); ?>
-						<?php if ( $my_query->have_posts()  ) : 	while ( $my_query->have_posts() ) : $my_query->the_post();
+						<?php while ( $my_query->have_posts() ) : $my_query->the_post();
 							$do_not_duplicate[] = $post->ID; //stores posts of this query in array
-							
-						global $wp_query;	 //necessary to show the tags 
-							$wp_query->in_the_loop = true; 
-							$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
-							<div id="post-<?php the_ID(); ?>" <?php post_class(''); ?>	>
+							?>
+							<div id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
 								<?php include("loop.boxes.php")?>
 							</div>
-						<?php endwhile; else: ?>
-					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+						<?php endwhile; ?>
 					<?php endif; ?>
-				</div>	
-				<div class="col-md-4">
-					<!-- Publications posts column-->
-					<h3><img src="<?php bloginfo('template_url'); ?>/images/icon-publication.png"/><br><?php icl_link_to_element_check(970, 'category'); ?></h3>
-					<?php global $more;    // Declare global $more (before the loop). "para que seguir leyendo funcione"
-					//mirar codigo madre en http://www.hashbangcode.com/blog/create-page-posts-wordpress-417.html
-					$args = array(
-					 'ignore_sticky_posts' => 	0,
-					 'posts_per_page'=>	5,
-					 'cat'=> icl_object_id_check(970, 'category'),
-					 'post__not_in' => $do_not_duplicate,
-						);
-		 
-					$my_query = new WP_Query($args); ?>
-						<?php if ( $my_query->have_posts() ) : 	while ( $my_query->have_posts() ) : $my_query->the_post(); 
-							
-						global $wp_query;	 //necessary to show the tags 
-							$wp_query->in_the_loop = true; 
-							$more = 0;       // Set (inside the loop) to display content above the more "seguir leyendo" tag. ?>
-							<div id="post-<?php the_ID(); ?>" <?php post_class(''); ?>	>
-								<?php include("loop.boxes.php")?>
-							</div>
-						<?php endwhile; else: ?>
-					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-					<?php endif; ?>
-				</div>	
+					</div>
+				<?php } ?>
 			</div>	
 			<div class="row">
 				<a href="blog" title="Go to blog" alt="Go to blog" class="btn btn-primary btn-lg pull-right"><?php _e('Read more Blog Posts','globalrec'); ?></a>

fd026f32cfb34e833e6912029f9b6c21ed857dc7
numeroteca <pablo@montera34.com>
2020-03-11 13:23:04 +0100

    replace twitter @globalrec_ list in sidebar home page

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,6 @@
 <!-- begin main sidebar -->
 <div id="main-sidebar" class="nav nav-tabs nav-stacked">
 		<?php dynamic_sidebar( 'main-sidebar-widget' ) ?>
-		<a class="twitter-timeline"  href="https://twitter.com/numeroteca/globalrec"  data-widget-id="401042762868326400">Tweets from @numeroteca/globalrec</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-
+    <a class="twitter-timeline" data-height="900" href="https://twitter.com/global_rec/lists/globalrec?ref_src=twsrc%5Etfw">A Twitter List by global_rec</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
 </div>
 <!-- end main sidebar -->

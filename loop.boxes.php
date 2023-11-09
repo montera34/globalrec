8a4a11dc7737b3bc67c19c1c84f512f749cf3b90
Alfonso Sánchez Uzábal <skotperez@voragine.net>
2023-11-09 07:56:42 +0100

    feat: basic documents archive template

--- loop.boxes.php
+++ loop.boxes.php
@@ -20,6 +20,8 @@ if ( $postType == 'country' ) {
 } else if ( $postType == 'global-meeting' ) {
 	$postTypeTitle = __('Global Meeting','globalrec');
 } else if ( $postType == 'waste-picker-org' ) {
+} else if ( $postType == 'document' ) {
+	$postTypeTitle = __('Document','globalrec');
 }
 else {
 }
@@ -37,18 +39,50 @@ if ( $postType == 'country' ) {
 	$postTypeIcon = 'dashicons-admin-site';
 } else if ( $postType == 'waste-picker-org' ) {
 	$postTypeIcon = 'groups-dashicon';
+} else if ( $postType == 'document' ) {
+	$postTypeIcon = 'dashicons-portfolio';
 } else if ( $postType == 'page' ) {
 	$postTypeIcon = 'dashicons-media-document';
 } else if ( $postType == 'post' ) {
 	$postTypeIcon = 'dashicons-format-aside';
 } else {
 }
+
+// document fields
+$dc_fs_out = '';
+if ( $postType == 'document' ) {
+	$dc = pods('document',$post->ID);
+	$dc_y = $dc->display('_dc_year');
+	$dc_y_out = ( !empty($dc_y) ) ? '<small>'.__('Publication year: ','globalrec').$dc_y.'</small>' : '';
+	$dc_f = $dc->field('_dc_file');
+	$dc_f_out = '';
+	if ( !empty($dc_f) ) {
+		$f_perma = $dc->display('_dc_file');
+		$f_size = size_format( filesize( get_attached_file( $dc_f['ID'] ) ) );
+		$f_mime = $dc_f['post_mime_type'];
+		if ( strpos( $f_mime, 'pdf' ) !== false ) {
+			$dc_f_l = __('Download: ','globalrec').'<span class="dashicons dashicons-pdf"></span> ('.$f_size.')';
+		}
+		elseif ( strpos( $f_mime, 'image' ) !== false ) {
+			$dc_f_l = __('Download: ','globalrec').'<span class="dashicons dashicons-format-image"></span> ('.$f_size.')';
+		}
+		else {
+			$dc_f_l = __('Download file','globalrec').' ('.$f_size.')';
+		}
+		$dc_f_out = '<div><small><a href="'.$f_perma.'">'.$dc_f_l.'</a></small></div>';
+	}
+	$dc_fs_out = '
+	<div class="dc-meta">'.
+		$dc_y_out.$dc_f_out
+	.'</div>
+	';
+}
 ?>
 
 <div class="thumbnail">
 	<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
 	<?php if (has_post_thumbnail()) :
-			echo "<div class=\"size-thumbnail\" style=\"margin:0 0 10px 0;\">";
+			echo '<div class="size-thumbnail" style="margin:0 0 10px 0;">';
 			//the_post_thumbnail( 'medium', array('class' => 'img-responsive') );
 			
 			if (get_the_post_thumbnail($post->ID,'medium') != '' ) {
@@ -132,6 +166,9 @@ if ( $postType == 'country' ) {
 			</small>
 		</div>
 	<?php }?>
+
+	<?php echo $dc_fs_out; ?>
+
 		<?php $text = get_post_meta( $post->ID, 'gm_location', true ); echo $text; ?> 
 		<div class="pull-right"> 
 			<span class="label "><?php echo get_the_term_list( $post->ID, 'gb-year', ' ', ', ', '' ); ?></span> 
